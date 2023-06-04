f62aecbad68a755015cf281c328d4a30e6368d1e
Mikołaj Strusiński <mikolaj.strusinski@medius.com>
2023-06-04 22:52:06 +0200

    unsafe dburl pasted

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-const connectUri = process.env.DB_URL;//"mongodb://127.0.0.1:27017/fileShare";
+const connectUri = "mongodb+srv://mikolaj:mikolaj3300@cluster0.kkpzzji.mongodb.net/"//process.env.DB_URL;//"mongodb://127.0.0.1:27017/fileShare";
 
 mongoose.connect( connectUri, { 
   useNewUrlParser: true, 
